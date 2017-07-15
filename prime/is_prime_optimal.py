cae4e3390ced9e80d84795fb6c2a04f3292b356f
Xaltonon <xaltonon@gmail.com>
2017-07-15 12:48:12 -0700

    Implemented rabin-miller prime test.
    This greatly improves the efficiency of is_prime_optimal for large
    numbers.

--- prime/is_prime_optimal.py
+++ prime/is_prime_optimal.py
@@ -1,8 +1,14 @@
 import math
+import random
 
+
+RABIN_MILLER_ROUNDS = 64        # 2^-128 chance of a false positive
+
+class ContinueOut(Exception):
+    pass
 
 def is_prime_optimal(n: int) -> bool:
-    """Decide the whether the given interger is prim number or not more efficiently
+    """Decide the whether the given interger is prime number or not more efficiently
 
     >>> is_prime_optimal(1)
     False
@@ -21,9 +27,37 @@ def is_prime_optimal(n: int) -> bool:
     """
     if n < 1:
         raise ValueError("n must be >= 1")
+    if math.floor(n) != n:
+        raise ValueError("n must be exact integer")
+
     if n == 1:
         return False
-    for i in range(2, int(math.sqrt(n) + 1)):
-        if n % i == 0:
-            return False
+    elif n in [2,3,5]:            # special case where n==2, 3 or 5
+        return True
+    elif n%2 == 0:              # even number
+        return False
+
+    d, r = divmod(n-1, 2)       # factor out 2s from n-1
+    s = 1
+    while r == 0:
+        s += 1
+        d, r = divmod(d, 2)
+    d = d*2+r
+    s -= 1
+
+    for _ in range(RABIN_MILLER_ROUNDS):
+        a = random.randint(2, n-2)
+        x = pow(a, d, n)
+        if x == 1 or x == n-1:
+            continue
+        try:
+            for _ in range(s-1):
+                x = pow(x, 2, n)
+                if x == 1:
+                    return False
+                if x == n-1:
+                    raise ContinueOut()
+        except ContinueOut:
+            continue
+        return False
     return True
