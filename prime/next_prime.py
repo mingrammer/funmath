f1fbaf57f347dc5af3f61966641910b2cafbcf52
TsimpDim <dimitristsimpitas@yahoo.gr>
2017-07-20 18:00:25 +0300

    Reorganised file for consistency

--- prime/next_prime.py
+++ prime/next_prime.py
@@ -1,15 +1,14 @@
 def next_prime(number:int):
- #Find the next prime after the input
- #   >>>next_prime(3)
- #   5
- #   >>>next_prime(1421)
- #   1423
- #   >>>next_prime(-26)
- #   ValueError: Number must be >= 1
- #   >>>next_prime(80023045)
- #   80023051
-
-
+    """Find the next prime after the input
+    >>>next_prime(3)
+    5
+    >>>next_prime(1421)
+    1423
+    >>>next_prime(-26)
+    ValueError: Number must be >= 1
+    >>>next_prime(80023045)
+    80023051
+    """
     if number <= 0 :   raise ValueError("Number must be >= 1")
     number = number + 1
     for p in range(number, 2*number):
@@ -18,5 +17,3 @@ def next_prime(number:int):
                 break
         else:
             return p
-
-
