4091ed70862d8cdbce21ec3553bd91adb7e971b8
TsimpDim <dimitristsimpitas@yahoo.gr>
2017-07-20 18:06:50 +0300

    Added blanck in docstring

--- prime/next_prime.py
+++ prime/next_prime.py
@@ -1,12 +1,12 @@
 def next_prime(number:int):
     """Find the next prime after the input
-    >>>next_prime(3)
+    >>> next_prime(3)
     5
-    >>>next_prime(1421)
+    >>> next_prime(1421)
     1423
-    >>>next_prime(-26)
+    >>> next_prime(-26)
     ValueError: Number must be >= 1
-    >>>next_prime(80023045)
+    >>> next_prime(80023045)
     80023051
     """
     if number <= 0 :   raise ValueError("Number must be >= 1")
