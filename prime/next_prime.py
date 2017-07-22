f459f5bae4c99d36f5b1317434c38bb5db25bf15
TsimpDim <dimitristsimpitas@yahoo.gr>
2017-07-22 14:00:18 +0300

    Added next_prime function (#5)
    * Added next_prime function
    
    * Fixed zero case
    
    * Update README.md
    
    * Reorganised file for consistency
    
    * Added blanck in docstring
    
    * Changed docstring
    
    * Updated Readme
    
    * Fixed docstring/actual error difference

---
+++ prime/next_prime.py
@@ -0,0 +1,21 @@
+def next_prime(number:int):
+    """Find the next prime after the input
+    >>> next_prime(3)
+    5
+    >>> next_prime(1421)
+    1423
+    >>> next_prime(-26)
+    Traceback (most recent call last):
+        ...
+    ValueError: Number must be >= 1
+    >>> next_prime(80023045)
+    80023051
+    """
+    if number <= 0 :   raise ValueError("Number must be >= 1")
+    number = number + 1
+    for p in range(number, 2*number):
+        for i in range(2, p):
+            if p % i == 0:
+                break
+        else:
+            return p
